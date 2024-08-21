d4965ad011ebe0d7ba417472a740930c8a63b9f4
Archit Sharma <74408634+iArchitSharma@users.noreply.github.com>
2024-08-21 16:10:39 +0000

    build script added
    Signed-off-by: Archit Sharma <74408634+iArchitSharma@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,28 +1,28 @@
+# Define the compiler and necessary tools
 CPP="g++"
 COMPILER="sukuna.cpp"
-CC="gcc"
 
 function comp {
-	BN=$(basename -s .sk $1)
-	TTOUTPUT=$(${CPP} ${COMPILER} -o sukuna $1 2>&1)
-    BINOUT=$(./sukuna ${inputFile})
-	if [ $? -ne 0 ]; then
-		echo "${TTOUTPUT}"
-	else
-		mv out.c ${BN}.c
-		CCOUTPUT=$(${CC} -o ${BN} ${BN}.c)
-		if [ $? -ne 0 ]; then
-			echo "${CCOUTPUT}"
-		else
-			echo "${TTOUTPUT}"
-		fi
-	fi
+    BN=$(basename -s .sk $1)  # Get the base name of the input file without the extension
+
+    # Compile the C++ code (sukuna.cpp)
+    TTOUTPUT=$(${CPP} ${COMPILER} -o sukuna 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "${TTOUTPUT}"  # Print the compiler output if there was an error
+    else
+        # Execute the compiled binary with the input file as an argument
+        BINOUT=$(./sukuna $1 2>&1)
+        if [ $? -ne 0 ]; then
+            echo "Execution Error: ${BINOUT}"
+        else
+            echo "${BINOUT}"  # Print the output of the execution
+        fi
+    fi
 }
 
 if [ $# -eq 0 ]; then
-	for i in $(ls examples/*.teeny); do
-		comp $i
-	done
+    echo "No input file provided."
+    echo "Usage: $0 <inputFile>"
 else
-	comp $1
+    comp $1  # Compile and execute the specific file provided as an argument
 fi
